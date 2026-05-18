d16d6f33dff0c4890dbd6a9d3d6c57c0606d352d
winstone-1 <winstonemuna404@gmail.com>
2026-05-19 00:19:28 +0300

    fix: redirect root URL to login page

--- core/urls.py
+++ core/urls.py
@@ -2,8 +2,10 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
+from django.shortcuts import redirect
 
 urlpatterns = [
+    path('', lambda request: redirect('login'), name='home'),
     path('admin/', admin.site.urls),
     path('accounts/', include('accounts.urls')),
     path('accounts/', include('allauth.urls')),
