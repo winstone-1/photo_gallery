c915f0ed4c6c69adc19a8beb4db04bf2d37a6ffb
winstone-1 <winstonemuna404@gmail.com>
2026-05-19 13:29:11 +0300

    fix: use CloudinaryField for profile picture

--- accounts/models.py
+++ accounts/models.py
@@ -1,17 +1,14 @@
 from django.contrib.auth.models import AbstractUser
 from django.db import models
+from cloudinary.models import CloudinaryField
 
 
 class CustomUser(AbstractUser):
     """Extended user model with bio and profile picture."""
-    
+
     email = models.EmailField(unique=True)
     bio = models.TextField(blank=True)
-    profile_picture = models.ImageField(
-        upload_to='profiles/',
-        blank=True,
-        null=True
-    )
+    profile_picture = CloudinaryField('image', blank=True, null=True)
 
     def __str__(self):
         return self.username
