3ba8e365894f7ad573ccf29cbe90b72bc5efe3e9
winstone-1 <winstonemuna404@gmail.com>
2026-05-19 12:18:43 +0300

    fix: remove superuser creation from build script

--- build.sh
+++ build.sh
@@ -4,10 +4,3 @@ set -o errexit
 pip install -r requirements.txt
 python manage.py collectstatic --no-input
 python manage.py migrate
-
-# Auto-create superuser from environment variables
-if [[ $CREATE_SUPERUSER ]]; then
-    python manage.py createsuperuser --no-input \
-        --username $DJANGO_SUPERUSER_USERNAME \
-        --email $DJANGO_SUPERUSER_EMAIL
-fi
